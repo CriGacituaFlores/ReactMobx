d4d83073bae184ac98859bf077bb0aa2cfe4d10e
Cristian <cri.gacitua.flores@gmail.com>
2017-11-20 16:29:33 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter';
+import App from './App';
+import Lienzo from './Lienzo'
+import './index.css';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+ReactDOM.render(
+  <Lienzo />,
+  document.getElementById('root')
+);
